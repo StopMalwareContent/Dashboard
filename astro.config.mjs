d6a53129b5d8f1b4f27d0a1499299de77620f41d
Nitrrine <nitrrine@team.lodine.xyz>
2025-01-07 20:04:50 +0500

    Add functional admin panel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,15 @@
 // @ts-check
-import { defineConfig } from "astro/config"
+import { defineConfig, envField } from "astro/config"
 
 // https://astro.build/config
-export default defineConfig({})
+export default defineConfig({
+  env: {
+    schema: {
+      API_URL: envField.string({
+        context: "client",
+        access: "public",
+        optional: false,
+      }),
+    },
+  },
+})
